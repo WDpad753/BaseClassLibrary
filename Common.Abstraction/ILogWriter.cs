3ff3e52772fc96a1d789feba930331f657afc840
Mohamed Ahmed <mohamed.ahmed@s3-id.com>
2025-06-13 13:14:55 +0100

    Trail for building the test project

--- Common.Abstraction/ILogWriter.cs
+++ Common.Abstraction/ILogWriter.cs
@@ -5,5 +5,8 @@ namespace Common.Abstractions
     public interface ILogWriter
     {
         void LogWrite(string message, string servicebase, string func, MessageLevels Messagelvl, DebugState debugLevel = 0);
+
+        //public string? LogFilePath { get; set; }
+        //public string? ConfigFilePath { get; set; }
     }
 }
