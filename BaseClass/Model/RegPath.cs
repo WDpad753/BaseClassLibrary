920d2f16368dd8a933e6eb9915af663561af71c0
Mohamed Ahmed <mohamed.ahmed@s3-id.com>
2025-08-06 16:02:55 +0100

    Modified Config UI.

---
+++ BaseClass/Model/RegPath.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseClass.Model
+{
+    public enum RegPath
+    {
+        User,
+        Machine
+    }
+}
